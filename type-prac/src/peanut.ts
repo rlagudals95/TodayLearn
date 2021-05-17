463cac8fed67c79b77eb5093f1c450279b52820a
rlagudals95 <76252074+rlagudals95@users.noreply.github.com>
2021-05-17 13:58:12 +0900

    0517

---
+++ type-prac/src/peanut.ts
@@ -0,0 +1,80 @@
+// 땅콩코딩 님의 강의
+
+let studentID: number = 1234;
+let studentName: string = 'hmk'
+let age: number = 21; // 다른 컴포넌트에서 선언해준 변수명과 겹치면 안돼는 것 같다...!
+let gender: string = 24;
+let subject: string = 'JavaScript'
+let courseCompleted: boolean = false
+
+let student1 = {
+    studentID  : 1234,
+    studentName  : 'hmk',
+    gender  : 'male',
+    subject  :'JavaScript',
+    courseCompleted : false,
+}
+
+
+// 함수 리턴값의 타입도 선언가능
+// function getStudentDetails(studentID: number) // :void 리턴값이 없음,  객체 object
+// :number   {
+//     return 1// void가 아닌 함수들은 타입에 맞는 return을 꼭해줘야 오류가 안뜬다
+
+// }
+
+// 인터페이스 사용하기(첫글자는 대문자로 하자!)
+
+interface Student { // 인터페이스로 정의 타입으로 사용가능
+   readonly studentID: number;  // readonly쓰면 읽기 전용 프로퍼티가되 수정이 불가능하게 됨
+    studentName: string;
+    age?: number, //age 프로퍼티가 있어도 되고 없어도 됨
+    gender: string,
+    subject: string,
+    courseCompleted: boolean
+    // addComment(comment: string): string,
+    addComment?:(comment: string) => string // 함수도 인터페이스로 지정가능
+}
+
+// 인터페이스를 타입으로 썼을때
+function getStudentDetails(studentID: number): Student // 아래처럼 객체를 길게 안써도 된다
+{
+       return {
+studentID  : 1234,
+studentName  : 'hmk',
+// 다른 컴포넌트에서 선언해준 변수명과 겹치면 안돼는 것 같다...!
+gender  : 'male',
+subject  :'JavaScript',
+courseCompleted : false,
+
+    }// void가 아닌 함수들은 타입에 맞는 return을 꼭해줘야 오류가 안뜬다
+}
+// 인터페이스를 타입으로 안썼을때
+
+function getStudentDetails2(studentID: number):  {
+        studentID: number;  //반환되는 객체의 구조를 타입으로 지정
+        studentName: string;
+        age?: number,
+        gender: string,
+        subject: string,
+        courseCompleted: boolean
+} {
+    return {
+studentID  : 1234,
+studentName  : 'hmk',
+gender  : 'male',
+subject  :'JavaScript',
+courseCompleted : false,
+    }// void가 아닌 함수들은 타입에 맞는 return을 꼭해줘야 오류가 안뜬다
+}
+
+// 근데 이렇게 함수 반환값을 객체구조로 했을때 코드가 너무 복잡하다! getStudentDetails함수 위처럼 해보자!
+
+
+// 코드의 재사용 용이!
+function saveStudentDetails(student: Student): void { // 인터페이스를 매게 변수로 가져옮
+    // student.studentID = 11222; //studentID를 바꾸는 함수
+    
+}
+
+saveStudentDetails(student1) //student1 위에서 변수로 지정!
